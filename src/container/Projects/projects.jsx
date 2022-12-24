87301ae7d0d446fcb005b336cab831e9cf3930d2
Kashif Usman <usmankashif544@gmail.com>
2022-12-24 23:24:53 +0500

    paragraph fix

--- src/container/Projects/projects.jsx
+++ src/container/Projects/projects.jsx
@@ -25,7 +25,7 @@ function projects() {
               alt='card' 
               text='Portfolio' 
               link='https://github.com/iamkashifkhn/portfolio1' 
-              subtext='Portfolio is my updated person portfolio website which is build by heart. It is open source and you are free to use the code as well from the Github link.'
+              subtext='Portfolio is my personal portfolio website which is build by heart. It is open source and you are free to use the code as well from the Github link.'
               tech='HTML5, CSS3, AntD, React'
               />
               <Card 

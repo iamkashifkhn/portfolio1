0c76adab8ae37640bae123247c1aea9fbe2f31c6
Kashif Usman <usmankashif544@gmail.com>
2023-02-01 11:59:24 +0500

    added animations

--- src/components/Cards/card.jsx
+++ src/components/Cards/card.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import './card.css'
+import { motion } from "framer-motion";
 
 function card(props) {
+
   return (
+    <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 1.5 }}
+        className="app__header-info"
+      >
     <div className='portfolio__card'>
         <div className='portfolio__card-content'>
         <img src={props.imageURL} alt='imageAlt'/>
         <h4>{props.text}</h4>
         </div>
     </div>
+    </motion.div>
   )
 }
 

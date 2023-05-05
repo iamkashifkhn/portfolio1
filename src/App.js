7a8ed42f5b1d663397ca37828dea0b7ba72ab04f
iamkashifkhn <usmankashif544@gmail.com>
2023-05-05 06:12:40 -0700

    fixed navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import './App.css';
-import {Navbar, Skills, Footer} from './components'
-import {Header, Services, Projects, About, Contacts} from './container'
-import { FloatButton } from 'antd';
+import "./App.css";
+import { Navbar, Skills, Footer } from "./components";
+import { Header, Services, Projects, About, Contacts } from "./container";
+import { FloatButton } from "antd";
 
 function App() {
   return (
     <div className="App">
-        <div>
-          <Navbar/>
-        </div>
-        <Header/>
-        <About/>
-        <Services/>
-        <Skills/>
-        <Projects/>
-        <Contacts/>
-        <Footer/>
-        <FloatButton.BackTop />
+      <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Skills />
+      <Projects />
+      <Contacts />
+      <Footer />
+      <FloatButton.BackTop />
     </div>
   );
 }
